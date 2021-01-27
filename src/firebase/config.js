3c40a2f54bb01d28ca3a1ff7ff1467ee4bbb5ea7
Ahmad Raza <39387314+ahmadrazach@users.noreply.github.com>
2021-01-27 23:04:41 +0500

    worked on firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCckdQBacQb731oeS0Lg0VQ9oal57l0c58",
+    authDomain: "hackernews-86174.firebaseapp.com",
+    projectId: "hackernews-86174",
+    storageBucket: "hackernews-86174.appspot.com",
+    messagingSenderId: "732530045757",
+    appId: "1:732530045757:web:d8e33f65fe9fa8caf34b54",
+    measurementId: "G-0RJ4EZC8HV"
+  };
+
+  export default firebaseConfig;
